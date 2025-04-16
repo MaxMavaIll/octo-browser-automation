f5189813e2ac2c190b3932b823d7fde3a9125096
Black1Matter <blackmatter19.70@gmail.com>
2025-04-16 19:15:34 +0300

    Ready to work on writing scripts

--- octo_browser_integration/octo_pyppeteer.py
+++ octo_browser_integration/octo_pyppeteer.py
@@ -10,6 +10,7 @@ from pyppeteer import launch, connect
 from captcha.api_captcha import CaptchaAPI
 from google_sheets_integration.google_sheets import GoogleSheets
 from octo_browser_integration.pyppeteer import PyppeteerHelper
+from logging.handlers import TimedRotatingFileHandler
 
 
 class OctoBrowserPyppeteer:
@@ -89,10 +90,10 @@ class OctoBrowserPyppeteer:
                 else:
                     return await response.text()
 
-    async def get_debug_port(self, profile_id):
+    async def get_debug_port(self, profile_id, name):
         for p in self.active_profiles:
             if p.get('uuid') == profile_id and p.get('state') == 'STARTED':
-                print(f"{profile_id} -> {p.get('debug_port')}")
+                self.log.info(f"{profile_id} | {name}-> {p.get('debug_port')}")
                 if p.get('debug_port'):
                     return p.get('debug_port')
                 await self.stop_octo_profile(profile_id)
@@ -133,25 +134,28 @@ class OctoBrowserPyppeteer:
         return page
 
     async def close_current_tabs(self, profile_id):
-        for page in self.pages[profile_id]:
-            await page.close()
-        del self.pages[profile_id]
+        if profile_id in self.pages:
+            for page in self.pages[profile_id]:
+                await page.close()
+            del self.pages[profile_id]
 
     def update_data(self, data: dict):
         file_path = os.path.join('data', 'data.json')
         with open(file_path, 'w') as file:
             json.dump(data, file)
 
-    def get_data(self):
-        file_path = os.path.join('data', 'data.json')
-        if not os.path.exists('data'):
-            os.mkdir('data')
+    def data_setup(self):
+        LOG_DIR = "data"
+        LOG_FILE = os.path.join(LOG_DIR, "data.json")
+
+        if not os.path.exists(LOG_DIR):
+            os.mkdir(LOG_DIR)
         
-        if not os.path.exists(file_path):
-            with open(file_path, 'w') as file:
+        if not os.path.exists(LOG_FILE):
+            with open(LOG_FILE, 'w') as file:
                 json.dump({}, file)
 
-        with open(file_path, 'r') as file:
+        with open(LOG_FILE, 'r') as file:
             result = json.load(file)
         
         return result
@@ -165,21 +169,36 @@ class OctoBrowserPyppeteer:
 
         return config_profiles
     
-    # def log_setup(self):
-    #     if not os.path.exists('logs'):
-    #         os.mkdir('logs')
+    def log_setup(self):
+        LOG_DIR = "logs"
+        LOG_FILE = os.path.join(LOG_DIR, "app.log")
 
-    #     log = logging.getLogger('octo')
-    #     log.setLevel(logging.DEBUG)
-    #     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    #     file_handler = logging.FileHandler('octo.log')
-    #     file_handler.setLevel(logging.DEBUG)
-    #     file_handler.setFormatter(formatter)
-    #     log.addHandler(file_handler)
-    #     return log
+        if not os.path.exists(LOG_DIR):
+            os.makedirs(LOG_DIR)
+
+        if not os.path.exists(LOG_FILE):
+            os.makedirs(LOG_DIR)
+
+        LOG_FORMAT = "%(asctime)s [%(levelname)s] %(name)s: %(message)s"
+
+        file_handler = TimedRotatingFileHandler(LOG_FILE, when="midnight", interval=1, backupCount=7, encoding="utf-8")
+        file_handler.setFormatter(logging.Formatter(LOG_FORMAT))
+        file_handler.setLevel(logging.DEBUG)
+
+        console_handler = logging.StreamHandler()
+        console_handler.setFormatter(logging.Formatter(LOG_FORMAT))
+        console_handler.setLevel(logging.INFO)
+
+        logger = logging.getLogger("octo")
+        logger.setLevel(logging.DEBUG)  # Мінімальний рівень логування
+        logger.addHandler(file_handler)
+        logger.addHandler(console_handler)
+        return logger
 
     def __init__(self, url_api: str, url_local_api: str, token: str):
         self.config = toml.load('config.toml')
+        self.data = self.data_setup()
+        self.log = self.log_setup()
         self.monitoring_version = False
         self.url_api = url_api
         self.url_local_api = url_local_api
@@ -189,9 +208,7 @@ class OctoBrowserPyppeteer:
         self.pages = {}
         self.gg_sheet = GoogleSheets(url_sheets=self.config['google_sheet']['api'])
         self.captcha = CaptchaAPI(api_key=self.config['2captcha']['api_key'])
-        self.helper_pyppeteer = PyppeteerHelper(catpcha=self.captcha)
-        self.data = self.get_data()
-        # self.log = self.log_setup()
+        self.helper_pyppeteer = PyppeteerHelper(catpcha=self.captcha, logger=self.log)
 
 
         network_accouts = self.gg_sheet.get_network_accout()
--- octo_browser_integration/pyppeteer.py
+++ octo_browser_integration/pyppeteer.py
@@ -2,12 +2,28 @@ import asyncio
 import random
 import sys
 import time
+import logging
 
 
 from captcha.api_captcha import CaptchaAPI
 from pyppeteer.page import Page
 
 class PyppeteerHelper:
+    async def safe_wait_for_xpath(self, page: Page, selector, timeout=20000):
+        try:
+            element = await page.waitForXPath(selector, timeout=timeout)
+            return element
+        except Exception as e :
+            self.log.error(f"Element not found by xpath:{selector}\nError: {e}")
+            return None
+        
+    async def safe_wait_for_selector(self, page: Page, selector, timeout=20000):
+        try:
+            element = await page.waitForSelector(selector, timeout=timeout)
+            return element
+        except Exception as e :
+            self.log.error(f"Element not found by xpath:{selector}\nError: {e}")
+            return None
 
     async def random_sleep(self, min_sec: int = 2, max_sec: int = 4):
         if min_sec >= max_sec:
@@ -15,29 +31,31 @@ class PyppeteerHelper:
             return
         
         sleep_time = random.uniform(min_sec, max_sec)
-        print(f"Sleeping for {sleep_time} seconds")
+        self.log.info(f"Sleeping for {sleep_time} seconds")
         await asyncio.sleep(sleep_time)
 
     async def enter_text(self, page: Page, selector: str, text: str, by: str = "css"):
         if by.lower() == "css":
-            await page.waitForSelector(selector)
-            await page.type(selector, text)
-        elif by.lower() == "xpath":
-            elements = await page.xpath(selector)
+            elements = await self.safe_wait_for_selector(selector)
             if elements:
-                await elements[0].type(text)
+                await page.type(selector, text)
+        elif by.lower() == "xpath":
+            elements = await self.safe_wait_for_xpath(page, selector)
+            if elements:
+                await elements.type(text)
         else:
             raise ValueError("Параметр 'by' повинен бути 'css' або 'xpath'")
         await self.random_sleep()
 
     async def click_element(self, page: Page, selector: str, by: str = "css"):
         if by.lower() == "css":
-            await page.waitForSelector(selector)
-            await page.click(selector)
-        elif by.lower() == "xpath":
-            elements = await page.xpath(selector)
+            elements = await self.safe_wait_for_selector(selector)
             if elements:
-                await elements[0].click()
+                await elements.click(selector)
+        elif by.lower() == "xpath":
+            elements = await self.safe_wait_for_xpath(page, selector)
+            if elements:
+                await elements.click()
         else:
             raise ValueError("Параметр 'by' повинен бути 'css' або 'xpath'")
         await self.random_sleep()
@@ -56,10 +74,9 @@ class PyppeteerHelper:
 
     async def clean_input_key(self, page: Page, selector: str, by: str = "css"):
         if by.lower() == "css":
-            await page.waitForSelector(selector)
-            element = await page.querySelector(selector)
+            element = await self.safe_wait_for_selector(page, selector)
         elif by.lower() == "xpath":
-            element = await page.waitForXPath(selector)
+            element = await self.safe_wait_for_xpath(page, selector)
         else:
             raise ValueError("Параметр 'by' повинен бути 'css' або 'xpath'")
 
@@ -74,50 +91,100 @@ class PyppeteerHelper:
 
     async def get_element_text(self, page: Page, selector: str, by: str = "css"):
         if by.lower() == "css":
-            element = await page.querySelector(selector)
+            element = await self.safe_wait_for_selector(page, selector)
             if not element:
                 return None
-            text = await page.evaluate('(el) => el?.innerText', await page.querySelector(selector))
+            text = await page.evaluate('(el) => el?.innerText', element)
         elif by.lower() == "xpath":
-            element = await page.xpath(selector)
+            element = await self.safe_wait_for_xpath(page, selector)
             if not element:
                 return None
-            text = await page.evaluate('(el) => el?.innerText', element[0])
+            text = await page.evaluate('(el) => el?.innerText', element)
 
         return text
 
+    async def test_js_injection(self, page: Page):
+        iframe_selector = 'iframe[src*="challenges.cloudflare.com"]'
+
+        iframe_handle = await page.evaluateHandle('''() => {
+            const shadowHost = document.querySelector('#uATa8');
+            if (!shadowHost) return null;
+            
+            let iframe = null;
+            if (shadowHost.shadowRoot) {
+                iframe = shadowHost.shadowRoot.querySelector('iframe[src*="challenges.cloudflare.com"]');
+            }
+            return iframe;
+        }''')
+
+        if iframe_handle:
+            iframe = iframe_handle.asElement()
+            iframe = await iframe.contentFrame()
+        else:
+            print("Iframe не знайдений")
+
     async def switch_to_iframe(self, page: Page, selector: str):
-        await page.waitForSelector(selector)
-        element = await page.querySelector(selector)
+        element = await self.safe_wait_for_selector(page, selector)
+        if not element:
+            return None
         frame = await element.contentFrame()
         return frame
+    
+    async def solve_cloudflare(self, page: Page, selector_block_checker: str, selector_check_mark: str):
+        anchor = await self.safe_wait_for_selector(page, selector_block_checker)
+        if not anchor:
+            return
+        
+        box = await anchor.boundingBox()
+        x = box['x'] + box['width'] * 0.08  # приблизно 8% від лівого краю
+        y = box['y'] + box['height'] * 0.5
+        await page.mouse.click(x, y)
 
-    async def solve_recaptcha(self, page: Page,  url_site):
-        recaptcha_frame = await self.switch_to_iframe(page, "iframe[src*='recaptcha']")
-        await recaptcha_frame.waitForSelector("#recaptcha-anchor")
-        anchor = await recaptcha_frame.querySelector("#recaptcha-anchor")
+    async def solve_recaptcha(
+            self, 
+            page: Page,  
+            url_site: str = None, 
+            selector_block_checker: str = "iframe[src*='recaptcha']", 
+            selector_check_mark: str = "#recaptcha-anchor",
+            name_checker: str = None
+            ):
+        """
+        EXAMPLE:
+        selector_block_checker = "iframe[src*='recaptcha']"
+        selector_check_mark = "#recaptcha-anchor"
+        """
+ 
+        
+        recaptcha_frame = await self.switch_to_iframe(page, selector_block_checker)
+        if not recaptcha_frame:
+            return
+        
+        anchor = await self.safe_wait_for_selector(page, selector_check_mark)
         await anchor.click()
         await self.random_sleep()
 
-        aria_checked = await recaptcha_frame.evaluate('(element) => element.getAttribute("aria-checked")', anchor)
-        
-        if aria_checked == "false":
-            with open('captcha/findRecaptchaClients.js', 'r', encoding='utf-8') as file:
-                script = file.read()
-            results = await page.evaluate(script)
+        if name_checker == "Recaptcha3":
+
+            aria_checked = await recaptcha_frame.evaluate('(element) => element.getAttribute("aria-checked")', anchor)
             
-            sitekey = results[0]['sitekey']
-            callback = results[0]['callback']
-            print(f"Sitekey: {sitekey}")
-            print(f"Callback: {callback}")
-            
-            captcha_solution = await self.captcha.RecaptchaV2(sitekey, url_site)
-            if captcha_solution is None:
-                return
-            
-            await page.evaluate(f"{callback}(\"{captcha_solution}\");")
+            if aria_checked == "false":
+                with open('captcha/findRecaptchaClients.js', 'r', encoding='utf-8') as file:
+                    script = file.read()
+                results = await page.evaluate(script)
+                
+                sitekey = results[0]['sitekey']
+                callback = results[0]['callback']
+                print(f"Sitekey: {sitekey}")
+                print(f"Callback: {callback}")
+                
+                captcha_solution = await self.captcha.RecaptchaV2(sitekey, url_site)
+                if captcha_solution is None:
+                    return
+                
+                await page.evaluate(f"{callback}(\"{captcha_solution}\");")
 
 
 
-    def __init__(self, catpcha: CaptchaAPI):
+    def __init__(self, catpcha: CaptchaAPI, logger: logging.Logger):
         self.captcha = catpcha
+        self.log = logger
